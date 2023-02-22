535d2bf3a286824d58b39d9aa362c26ac0be12c4
GeeKiana31 <klgee1@uci.edu>
2023-02-21 16:12:52 -0800

    Operation overload and plot methods

--- SignalDetection.m
+++ SignalDetection.m
@@ -31,5 +31,36 @@ classdef SignalDetection
         function Criterion = Criterion(obj)
             Criterion = -0.5 * (norminv(obj.HitRate) + norminv(obj.FARate));
         end
+
+%% Operation Override 
+        function Total = plus(obj1, obj2)
+            Total = Vector(obj1.Hits + obj2.Hits, obj1.Misses + ...
+                obj2.Misses, obj1.FalseAlarms + obj2.FalseAlarms, ...
+                obj1.CorrectRejections + obj2.CorrectRejections);
+        end
+
+        function scaled = times(obj, k)
+            scaled = Vector(obj.Hits .* k, obj.Misses .* k, ...
+                obj.FalseAlarms .* k, obj.CorrectRejections .* k);
+        end
+
+%% Generate Plots
+        function plot_roc = plot_roc(obj)
+            plot_roc = plot(obj.HitRate, obj.FARate);
+        end
+
+        function plot_sdt = plot_sdt(obj)
+            x = [-10:2:20];
+            Noise = normpdf(x,0,1);
+            Signal = normpdf(x,obj.D_Prime,1);
+            plot(x,Noise)
+
+            hold on
+
+            plot(x,Signal)
+            line(x,obj.D_Prime)
+
+            hold off
+        end
     end
 end
