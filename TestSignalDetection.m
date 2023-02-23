d6c92d6c791e37db89f138030e090fc4ba3e9e2d
GeeKiana31 <klgee1@uci.edu>
2023-02-22 16:54:20 -0800

    update corruption test again

--- TestSignalDetection.m
+++ TestSignalDetection.m
@@ -5,7 +5,7 @@ end
 function testDPrimeZero(testCase)
 
   obj      = SignalDetection(15, 5, 15, 5);
-  actual   = obj.d_prime();
+  actual   = obj.D_Prime();
   expected = 0;
   verifyEqual(testCase, actual, expected, 'AbsTol', 1e-6);
   
@@ -14,7 +14,7 @@ end
 function testDPrimeNonzero(testCase)
   
   obj      = SignalDetection(15, 10, 15, 5);
-  actual   = obj.d_prime();
+  actual   = obj.D_Prime();
   expected = -0.421142647060282;
   verifyEqual(testCase, actual, expected, 'AbsTol', 1e-6);
   
@@ -23,7 +23,7 @@ end
 function testCriterionZero(testCase)
 
   obj = SignalDetection(5, 5, 5, 5);
-  actual = obj.criterion();
+  actual = obj.Criterion();
   expected = 0;
   testCase.verifyEqual(actual, expected, 'AbsTol', 1e-6);
   
@@ -32,7 +32,7 @@ end
 function testCriterionNonzero(testCase)
 
   obj = SignalDetection(15, 10, 15, 5);
-  actual = obj.criterion();
+  actual = obj.Criterion();
   expected = -0.463918426665941;
   testCase.verifyEqual(actual, expected, 'AbsTol', 1e-6);
   
@@ -42,15 +42,12 @@ end
 function testCorruption(testCase)
 
     obj = SignalDetection(5, 5, 5, 5);
-    actual = obj.criterion();
-    expected = 0;
-    testCase.verifyEqual(actual, expected, 'AbsTol', 1e-6);
+    actual = obj.Criterion();
     obj.Hits = 15;
     obj.Misses = 10;
     obj.FalseAlarms = 15;
     obj.CorrectRejections = 5;
-    actual = obj.criterion();
-    expected = 0;
+    expected = obj.Criterion();
     testCase.verifyEqual(actual, expected, 'AbsTol', 1e-6);
 
 end
@@ -59,8 +56,8 @@ end
 function testAddition(testCase)
 
   obj = SignalDetection(1, 1, 2, 1) + SignalDetection(2, 1, 1, 3);
-  actual   = obj.criterion();
-  expected = SignalDetection(3, 2, 3, 4).criterion;
+  actual   = obj.Criterion();
+  expected = SignalDetection(3, 2, 3, 4).Criterion;
   testCase.verifyEqual(actual, expected, 'AbsTol', 1e-6);
 
 end
@@ -68,8 +65,8 @@ end
 function testMultiplication(testCase)
 
   obj = SignalDetection(1, 2, 3, 1) * 4;
-  actual   = obj.criterion();
-  expected = SignalDetection(4, 8, 12, 4).criterion;
+  actual   = obj.Criterion();
+  expected = SignalDetection(4, 8, 12, 4).Criterion;
   testCase.verifyEqual(actual, expected, 'AbsTol', 1e-6);
 
 end
